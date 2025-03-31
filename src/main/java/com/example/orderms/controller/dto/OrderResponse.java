c2abd4e5df334dd59d5201982e8f2defff7bc41c
Kleber <63799768+kleber5000@users.noreply.github.com>
2025-03-31 19:04:16 -0300

    Primeiro commit

---
+++ src/main/java/com/example/orderms/controller/dto/OrderResponse.java
@@ -0,0 +1,14 @@
+package com.example.orderms.controller.dto;
+
+import com.example.orderms.entity.OrderEntity;
+
+import java.math.BigDecimal;
+
+public record OrderResponse(Long orderId,
+                            Long customerId,
+                            BigDecimal total) {
+
+    public static OrderResponse fromEntity(OrderEntity entity) {
+        return new OrderResponse(entity.getOrderId(), entity.getCustomerId(), entity.getTotal());
+    }
+}
---
+++ src/main/java/com/example/orderms/listener/OrderCreatedListener.java
@@ -0,0 +1,31 @@
+package com.example.orderms.listener;
+
+import com.example.orderms.listener.dto.OrderCreatedEvent;
+import com.example.orderms.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+import static com.example.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
+
+@Component
+public class OrderCreatedListener {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    private final OrderService orderService;
+
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listen(Message<OrderCreatedEvent> message) {
+        logger.info("Message consumed: {}", message);
+
+        orderService.save(message.getPayload());
+    }
+}
